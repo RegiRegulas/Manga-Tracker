42aae9cdd848e6823badc09133106df0cf1c9d73
Regulas <milindmishra2901@gmail.com>
2025-06-27 15:10:16 +0530

    Fix DB connection for Render

--- server/db.js
+++ server/db.js
@@ -7,11 +7,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render PostgreSQL
+  },
 });
 
 export default pool;
+
